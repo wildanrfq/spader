d9f565f349acce44bdf348032865558fab91d0fa
Wildan Rifqi <wildanrfqi@gmail.com>
2025-10-18 02:09:20 +0700

    Initial Commit

---
+++ spader/spaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaderApp.swift
+//  spader
+//
+//  Created by Wildan Rifqi on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct spaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
